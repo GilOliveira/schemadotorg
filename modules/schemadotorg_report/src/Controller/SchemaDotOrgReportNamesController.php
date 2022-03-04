430eb12e480f815acc8f1929d05b8c934a3a9737
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2022-03-04 11:41:45 -0500

    Create inital names usage report.

--- modules/schemadotorg_report/src/Controller/SchemaDotOrgReportNamesController.php
+++ modules/schemadotorg_report/src/Controller/SchemaDotOrgReportNamesController.php
@@ -12,7 +12,7 @@ class SchemaDotOrgReportNamesController extends SchemaDotOrgReportControllerBase
   /**
    * The Schema.org Names service.
    *
-   * @var \Drupal\schemadotorg\SchemaDotOrgManagerInterface
+   * @var \Drupal\schemadotorg\SchemaDotOrgNamesInterface
    */
   protected $schemaDotOrgNames;
 
@@ -26,12 +26,133 @@ class SchemaDotOrgReportNamesController extends SchemaDotOrgReportControllerBase
   }
 
   /**
+   * Builds the Schema.org names overview or table.
+   *
+   * @return array
+   *   A renderable array containing Schema.org names overview or table.
+   */
+  public function index($display) {
+    if ($display === 'overview') {
+      return $this->overview();
+    }
+    else {
+      return $this->table($display);
+    }
+  }
+
+  /**
+   * Builds the Schema.org names overview.
+   *
+   * @return array
+   *   A renderable array containing Schema.org names overview.
+   */
+  public function overview() {
+    $types_query = $this->database->select('schemadotorg_types', 't')->fields('t', ['label']);
+    $properties_query = $this->database->select('schemadotorg_properties', 'p')->fields('p', ['label']);
+    $labels = $types_query->union($properties_query)->orderBy('label')->execute()->fetchCol();
+
+    $prefixes = [];
+    $suffixes = [];
+    $words = [];
+    foreach ($labels as $label) {
+      $name = $this->schemaDotOrgNames->camelCaseToSnakeCase($label);
+      $name_parts = explode('_', $name);
+
+      $suffix = end($name_parts);
+      $suffixes += [$suffix => 0];
+      $suffixes[$suffix]++;
+
+      $prefix = reset($name_parts);
+      $prefixes += [$prefix => 0];
+      $prefixes[$prefix]++;
+
+      foreach ($name_parts as $name_part) {
+        $words += [$name_part => 0];
+        $words[$name_part]++;
+      }
+    }
+
+    $build = [];
+
+    $build['summary'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Usage summary'),
+      '#open' => TRUE,
+    ];
+
+    $build['summary']['words'] = $this->buildWordUsage(
+      $this->t('Words'),
+      $this->t('Word'),
+      $words,
+      $this->schemaDotOrgNames->getNameAbbreviations()
+    );
+
+    $build['summary']['prefixes'] = $this->buildWordUsage(
+      $this->t('Prefixes'),
+      $this->t('Prefix'),
+      $prefixes,
+      $this->schemaDotOrgNames->getNamePrefixes()
+    );
+
+    $build['summary']['suffixes'] = $this->buildWordUsage(
+      $this->t('Suffixes'),
+      $this->t('Suffix'),
+      $prefixes,
+      $this->schemaDotOrgNames->getNameSuffixes()
+    );
+
+    return $build;
+  }
+
+  protected function buildWordUsage($title, $label, array $words, array $abbreviations) {
+    // Remove words that are less than 5 characters.
+    $words = array_filter($words, function ($word) {
+      return strlen($word) > 5;
+    }, ARRAY_FILTER_USE_KEY);
+    // Remove words that are only used once.
+    $words = array_filter($words, function ($usage) {
+      return $usage > 1;
+    });
+    // Sort by usage.
+    asort($words, SORT_NUMERIC);
+    $words = array_reverse($words);
+
+    // Header.
+    $header = [
+      'word' => $label,
+      'word_usage' => $this->t('Used'),
+      'abbreviation' => $this->t('Abbreviation'),
+    ];
+
+    // Rows.
+    $rows = [];
+    foreach ($words as $word => $usage) {
+      $row = [];
+      $row['word'] = $word;
+      $row['word_usage'] = $usage;
+      $row['abbreviation'] = $abbreviations[$word] ?? '';
+      $rows[] = $row;
+    }
+
+    $build = [
+      '#type' => 'details',
+      '#title' => $title,
+    ];
+    $build['table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+    return $build;
+  }
+
+  /**
    * Builds the Schema.org names table.
    *
    * @return array
    *   A renderable array containing Schema.org names table.
    */
-  public function index($display = '') {
+  public function table($display) {
     $tables = ['types', 'properties'];
     $is_schema_item = in_array($display, $tables);
 
