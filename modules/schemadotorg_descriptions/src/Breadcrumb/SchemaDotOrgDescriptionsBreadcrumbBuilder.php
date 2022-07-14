e1a56bd527b8c6bde30766c5fabe59c2cbad14f0
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2022-07-14 05:40:54 -0400

    Add breadcrumb to descriptions and minor refactoriing.

---
+++ modules/schemadotorg_descriptions/src/Breadcrumb/SchemaDotOrgDescriptionsBreadcrumbBuilder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\schemadotorg_descriptions\Breadcrumb;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\schemadotorg\Breadcrumb\SchemaDotOrgBreadcrumbBuilder;
+
+/**
+ * Provides a breadcrumb builder for Schema.org descriptions.
+ */
+class SchemaDotOrgDescriptionsBreadcrumbBuilder extends SchemaDotOrgBreadcrumbBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    return ($route_match->getRouteName() === 'schemadotorg_descriptions.settings');
+  }
+
+}
--- modules/schemadotorg_jsonld/src/Breadcrumb/SchemaDotOrgJsonLdBreadcrumbBuilder.php
+++ modules/schemadotorg_jsonld/src/Breadcrumb/SchemaDotOrgJsonLdBreadcrumbBuilder.php
@@ -2,41 +2,19 @@
 
 namespace Drupal\schemadotorg_jsonld\Breadcrumb;
 
-use Drupal\Core\Breadcrumb\Breadcrumb;
-use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
-use Drupal\Core\Link;
 use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\schemadotorg\Breadcrumb\SchemaDotOrgBreadcrumbBuilder;
 
 /**
  * Provides a breadcrumb builder for Schema.org JSON-LD.
  */
-class SchemaDotOrgJsonLdBreadcrumbBuilder implements BreadcrumbBuilderInterface {
-  use StringTranslationTrait;
+class SchemaDotOrgJsonLdBreadcrumbBuilder extends SchemaDotOrgBreadcrumbBuilder {
 
   /**
    * {@inheritdoc}
    */
   public function applies(RouteMatchInterface $route_match) {
     return ($route_match->getRouteName() === 'schemadotorg_jsonld.settings');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function build(RouteMatchInterface $route_match) {
-    $breadcrumb = new Breadcrumb();
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Home'), '<front>'));
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Administration'), 'system.admin'));
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Configuration'), 'system.admin_config'));
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Search and metadata'), 'system.admin_config_search'));
-    $breadcrumb->addLink(Link::createFromRoute($this->t('Schema.org'), 'entity.schemadotorg_mapping.collection'));
-
-    // This breadcrumb builder is based on a route parameter, and hence it
-    // depends on the 'route' cache context.
-    $breadcrumb->addCacheContexts(['route']);
-
-    return $breadcrumb;
   }
 
 }
