4627a2bc2ddff16cd58a07f7a540d5d089eaafb7
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2022-09-10 08:48:43 -0400

    Clear local storage to reset the state of all details widget for demos.

--- modules/schemadotorg_demo/modules/schemadotorg_demo_standard_admin/js/schemadotorg_demo_standard_admin.js
+++ modules/schemadotorg_demo/modules/schemadotorg_demo_standard_admin/js/schemadotorg_demo_standard_admin.js
@@ -7,6 +7,10 @@
 
   'use strict';
 
+  // Clear local storage to reset the state of all details widget for demos.
+  // @see schemadotorg/js/schemadotorg.details.js
+  localStorage.clear();
+
   // Redirect anonymous users to /user/login.
   // Using JavaScript so that Acquia's Cloud IDE's 'share' query parameter
   // generates the 'share' cookie as expected.
