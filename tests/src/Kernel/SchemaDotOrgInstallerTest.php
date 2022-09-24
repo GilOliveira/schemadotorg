2f476cc6e317831830bd9d48d39180f822c362a9
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2022-09-24 17:49:11 -0400

    Refactor tests.

--- tests/src/Kernel/SchemaDotOrgInstallerTest.php
+++ tests/src/Kernel/SchemaDotOrgInstallerTest.php
@@ -18,12 +18,20 @@ class SchemaDotOrgInstallerTest extends SchemaDotOrgKernelTestBase {
   protected $installer;
 
   /**
+   * The Schema.org mapping type storage.
+   *
+   * @var \Drupal\schemadotorg\SchemaDotOrgMappingTypeStorageInterface
+   */
+  protected $mappingTypeStorage;
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
 
     $this->installer = $this->container->get('schemadotorg.installer');
+    $this->mappingTypeStorage = $this->container->get('entity_type.manager')->getStorage('schemadotorg_mapping_type');
   }
 
   /**
@@ -32,9 +40,15 @@ class SchemaDotOrgInstallerTest extends SchemaDotOrgKernelTestBase {
    * @covers ::requirements
    */
   public function testRequirements() {
+    // Check installation requirements.
     $requirements = $this->installer->requirements('runtime');
     $this->assertNotEmpty($requirements);
     $this->assertEquals('Schema.org Blueprints: Recommended modules missing', $requirements['schemadotorg_modules']['title']);
+
+    // Check creating mapping types for modules that provide a content entities.
+    $this->assertNull($this->mappingTypeStorage->load('storage'));
+    $this->installer->installModules(['storage']);
+    $this->assertNotNull($this->mappingTypeStorage->load('storage'));
   }
 
 }
