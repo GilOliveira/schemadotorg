62a07172d63ffcea0be984865f773ed704925913
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2022-10-11 08:51:57 -0400

    Fix broken tests

--- modules/schemadotorg_media/tests/src/Functional/SchemaDotOrgMediaSettingsFormTest.php
+++ modules/schemadotorg_media/tests/src/Functional/SchemaDotOrgMediaSettingsFormTest.php
@@ -36,7 +36,7 @@ class SchemaDotOrgMediaSettingsFormTest extends SchemaDotOrgBrowserTestBase {
    * Test Schema.org media settings form.
    */
   public function testSettingsForm(): void {
-    $this->assertSaveSettingsConfigForm('schemadotorg_media.settings', '/admin/config/search/schemadotorg/settings/typeså´');
+    $this->assertSaveSettingsConfigForm('schemadotorg_media.settings', '/admin/config/search/schemadotorg/settings/types');
     $this->assertSaveSettingsConfigForm('schemadotorg_media.settings', '/admin/config/search/schemadotorg/settings/properties');
   }
 
