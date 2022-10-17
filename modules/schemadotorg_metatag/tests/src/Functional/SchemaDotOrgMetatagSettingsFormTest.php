45f581a80596e0fe1ad56a8a62bf38426621e349
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2022-10-17 17:40:28 -0400

    Allowed meta tags and robots directives to be limited.

---
+++ modules/schemadotorg_metatag/tests/src/Functional/SchemaDotOrgMetatagSettingsFormTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\schemadotorg_metatag\Functional;
+
+use Drupal\Tests\schemadotorg\Functional\SchemaDotOrgBrowserTestBase;
+
+/**
+ * Tests the functionality of the Schema.org metatag settings form.
+ *
+ * @group schemadotorg
+ */
+class SchemaDotOrgMetatagSettingsFormTest extends SchemaDotOrgBrowserTestBase {
+
+  // phpcs:disable
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['schemadotorg_metatag'];
+  // phpcs:enable
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $account = $this->drupalCreateUser(['administer schemadotorg']);
+    $this->drupalLogin($account);
+  }
+
+  /**
+   * Test Schema.org metatag settings form.
+   */
+  public function testSettingsForm(): void {
+    $this->assertSaveSettingsConfigForm('schemadotorg_metatag.settings', '/admin/config/search/schemadotorg/settings/properties');
+  }
+
+}
