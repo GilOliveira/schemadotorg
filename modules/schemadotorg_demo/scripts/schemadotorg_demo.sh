921322dfcd105255a9fa081fbaae24d97fc14e54
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2022-11-04 04:01:15 -0400

    Improve schemadotorg_demo.sh

--- modules/schemadotorg_demo/scripts/schemadotorg_demo.sh
+++ modules/schemadotorg_demo/scripts/schemadotorg_demo.sh
@@ -7,14 +7,26 @@ function function_exists() {
 function help() {
   echo "schemadotorg_demo.sh";
   echo;
-  echo "Installs a demo of the Schema.org Blueprints module."
+  echo "Installs a demo of the Schema.org Blueprints module.";
   echo;
-  echo "This scripts assumes you are starting with a plain vanilla standard instance of Drupal."
+  echo "This scripts assumes you are starting with a plain vanilla standard instance of Drupal.";
+  echo;
+  echo "The below commands should be executed from the root of your Drupal installation.";
+  echo;
+  echo "Usage:"
+  echo;
+  IFS=$'\n'
+  for f in $(declare -F); do
+    function_name=${f:11}
+    if [[ ! $function_name =~ ^(help|function_exists)$ ]]; then
+      echo "./schemadotorg_demo.sh $function_name";
+    fi
+  done
 }
 
 function install() {
-  local profile=${1:-"standard"}
-  local email=`git config user.email`
+  local profile=${1:-"standard"};
+  local email=`git config user.email`;
 
   echo "Installing $profile profile";
   drush -yv site-install --account-mail="$email"\
@@ -24,13 +36,11 @@ function install() {
     --site-name="Schema.org Blueprints Demo"\
     $profile;
 
-  drush -y config-set system.site slogan 'A demo of the Schema.org Blueprints module for Drupal.'
+  drush -y config-set system.site slogan 'A demo of the Schema.org Blueprints module for Drupal.';
 }
 
 function install_base() {
-  install
-
-  # Base.
+  install;
   drush -y pm:enable schemadotorg\
     schemadotorg_descriptions\
     schemadotorg_devel\
@@ -42,8 +52,10 @@ function install_base() {
     schemadotorg_subtype\
     schemadotorg_taxonomy\
     schemadotorg_ui;
+}
 
-  # JSON:API/JSON-LD.
+function install_apis() {
+  install_base;
   drush -y pm:enable schemadotorg_jsonapi\
     schemadotorg_jsonapi_preview\
     schemadotorg_jsonld\
@@ -54,74 +66,69 @@ function install_base() {
 }
 
 function install_extras() {
-  install
-  drush -y pm:enable schemadotorg\
-    schemadotorg_action\
+  install_apis;
+  drush -y pm:enable schemadotorg_action\
     schemadotorg_auto_entitylabel\
     schemadotorg_content_moderation\
-    schemadotorg_descriptions\
-    schemadotorg_devel\
-    schemadotorg_export\
     schemadotorg_field_group\
     schemadotorg_flexfield\
     schemadotorg_focal_point\
     schemadotorg_inline_entity_form\
-    schemadotorg_jsonapi\
-    schemadotorg_jsonapi_preview\
-    schemadotorg_jsonld\
-    schemadotorg_jsonld_breadcrumb\
-    schemadotorg_jsonld_embed\
-    schemadotorg_jsonld_endpoint\
-    schemadotorg_jsonld_preview\
     schemadotorg_layout_paragraphs\
-    schemadotorg_mapping_set\
-    schemadotorg_media\
     schemadotorg_metatag\
     schemadotorg_office_hours\
-    schemadotorg_paragraphs\
-    schemadotorg_report\
     schemadotorg_scheduler\
     schemadotorg_sidebar\
     schemadotorg_simple_sitemap\
-    schemadotorg_smart_date\
-    schemadotorg_subtype\
-    schemadotorg_taxonomy\
-    schemadotorg_ui;
+    schemadotorg_smart_date;
 }
 
+function install_next() {
+  install_extras;
+  drush -y pm:enable schemadotorg_next\
+    schemadotorg_next_components;
+}
+
+################################################################################
+
 function install_demo_standard() {
-  install
+  install;
   drush -y pm:enable schemadotorg_demo_standard;
 }
 
-function install_demo_standard_translation() {
-  install_demo_standard
-  drush -y pm:enable schemadotorg_demo_standard_translation;
+function install_demo_standard_next() {
+  install_demo_standard;
+  drush -y pm:enable schemadotorg_demo_next;
 }
 
-function install_demo_standard_next() {
-  install_demo_standard
-  drush -y pm:enable schemadotorg_demo_next;
+function install_demo_standard_translation() {
+  install_demo_standard;
+  drush -y pm:enable schemadotorg_demo_standard_translation;
 }
 
 function install_demo_standard_translation_umami() {
   install_demo_standard_translation
 
-  drush locale:check
-  drush locale:update
+  drush locale:check;
+  drush locale:update;
   drush -y pm:enable schemadotorg_demo_umami_content;
 }
 
 ################################################################################
 
-SCRIPT_DIRECTORY=`dirname "$0"`
-
 function_name=$1; shift;
 
 if function_exists $function_name; then
+
   $function_name $@;
+
 else
-  echo "Function to '$function_name' does not exist.";
-  echo;
+
+  if [[ ! -z "$function_name" ]]; then
+    echo "Function to '$function_name' does not exist.";
+    echo;
+  fi
+
   help;
+
 fi
