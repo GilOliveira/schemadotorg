d79273f7e4b55e41c7abd21e6d701c40ee313120
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2023-03-19 09:37:00 -0400

    Create mappings for existing media types during installation.

---
+++ modules/schemadotorg_media/tests/src/Kernel/SchemaDotOrgMediaInstallTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\schemadotorg_media\Kernel;
+
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\language\Entity\ContentLanguageSettings;
+use Drupal\media\Entity\MediaType;
+use Drupal\schemadotorg\Entity\SchemaDotOrgMapping;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Tests\schemadotorg\Kernel\SchemaDotOrgKernelEntityTestBase;
+
+/**
+ * Tests the functionality of the Schema.org media installation.
+ *
+ * @covers \schemadotorg_media_install()
+ * @group schemadotorg
+ */
+class SchemaDotOrgMediaInstallTest extends SchemaDotOrgKernelEntityTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var string[]
+   */
+  protected static $modules = [
+    'media',
+    'schemadotorg_media',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->installEntitySchema('media');
+    $this->installConfig(['schemadotorg_media']);
+  }
+
+  /**
+   * Test Schema.org media installation.
+   */
+  public function testInstall(): void {
+    MediaType::create([
+      'id' => 'image',
+      'label' => 'Image',
+      'source' => 'image',
+    ])->save();
+
+    \Drupal::moduleHandler()->loadInclude('schemadotorg_media', 'install');
+
+    schemadotorg_media_install(FALSE);
+
+    /** @var \Drupal\schemadotorg\SchemaDotOrgMappingInterface $mapping */
+    $mapping = SchemaDotOrgMapping::load('media.image');
+
+    // Confirm media.image mapping is created an mapped to ImageObject.
+    $this->assertEquals('ImageObject', $mapping->getSchemaType());
+  }
+
+}
