072c61e778809629af42ad1b0cbcb4373f3a429b
Jacob Rockowitz <jrockowitz@users.noreply.github.com>
2023-04-25 15:58:47 -0400

    Linting

--- tests/src/Kernel/SchemaDotOrgMappingManagerTest.php
+++ tests/src/Kernel/SchemaDotOrgMappingManagerTest.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\schemadotorg\Kernel;
 
-use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\schemadotorg\Entity\SchemaDotOrgMapping;
 use Drupal\schemadotorg\SchemaDotOrgEntityFieldManagerInterface;
 
